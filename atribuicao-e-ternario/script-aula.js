8e2b26722d409f3b490c1ae9eb121bbaca28c791
Isabella Campos <isabellaszcampos@gmail.com>
2021-11-16 00:33:06 -0300

    escopo-e-atribuicao-e-tenario

---
+++ atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,7 @@
+const numero = 20;
+numero += 10; // numero = numero + 10;
+numero *= 10; // numero = numero * 10;
+
+const idade = 20;
+const podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+// condição ? true : false
